43869ececff43ce0eecbabcf2c76f13a5d6dbe4b
John Conley <john.conley@shawinc.com>
2020-09-19 01:52:35 -0400

    added new scripts and sorted.

--- Modules/ShawUtil/ShawUtil.psm1
+++ Modules/ShawUtil/ShawUtil.psm1
@@ -1,2 +1,4 @@
-. $psScriptRoot\Resize-VMDrive.ps1
 . $psScriptRoot\Get-ComputerMemoryUsage.ps1
+. $psScriptRoot\Get-LoggedOnUsers.ps1
+. $psScriptRoot\Repair-Wsus.ps1
+. $psScriptRoot\Resize-VMDrive.ps1
